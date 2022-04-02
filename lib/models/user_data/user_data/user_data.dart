c982b5c990c1b6bc192877feb30f8eb5f777207a
Truffel228 <75166573+Truffel228@users.noreply.github.com>
2022-04-02 11:59:09 +0300

    deleted toJson fromJson methods from user_data model

--- lib/models/user_data/user_data/user_data.dart
+++ lib/models/user_data/user_data/user_data.dart
@@ -2,8 +2,6 @@ import 'package:fire_base_app/models/map_comment/map_comment.dart';
 import 'package:fire_base_app/models/user_data/user_data_api/user_data_api.dart';
 import 'package:json_annotation/json_annotation.dart';
 
-part 'user_data.g.dart';
-
 @JsonSerializable()
 class UserData {
   UserData({required this.name, required this.age, required this.mapComments});
@@ -11,10 +9,6 @@ class UserData {
   final String name;
   final int age;
   final List<MapComment> mapComments;
-
-  factory UserData.fromJson(Map<String, dynamic> json) =>
-      _$UserDataFromJson(json);
-  Map<String, dynamic> toJson() => _$UserDataToJson(this);
 
   UserDataApi toApi() => UserDataApi(
       name: name,
